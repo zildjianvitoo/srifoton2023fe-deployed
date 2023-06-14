46906a04e23505aadf68a5be56219fb9c286effa
ZildjianVito <zildjianvito07@gmail.com>
2023-06-14 17:36:16 +0700

    make seminar jumbotron page

---
+++ components/Seminar/Jumbotron.jsx
@@ -0,0 +1,56 @@
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+export default function Jumbotron() {
+  const { theme } = useTheme();
+
+  return (
+    <section
+      id="#"
+      className="relative overflow-hidden h-full  bg-gradient-to-b  from-[#E5AFE5] to-[#337EF0] dark:from-[#E5AFE5] dark:to-[#1AB5E4] lg:h-screen opacity-90 mt-12 lg:mt-6"
+    >
+      <div
+        className="absolute inset-0 h-full lg:h-screen bg-center bg-no-repeat bg-cover opacity-60 z-[-1] "
+        style={{
+          backgroundImage: "url(assets/Seminar/jumbotron-seminar-bgimg.svg)",
+        }}
+      ></div>
+      <div className="flex flex-col-reverse w-full h-full gap-20 p-10 mt-8 lg:mt-16 lg:flex-row">
+        <div className="flex items-center  lg:w-[50%] ">
+          <Image
+            width={600}
+            height={580}
+            src="/assets/Seminar/jumbotron-seminar-img.png"
+            alt="Gambar robot"
+            quality={100}
+          />
+        </div>
+        <div className="flex flex-col gap-6 text-white lg:mt-24 lg:w-[70%]">
+          <h1 className="pb-2 text-4xl font-bold lg:leading-[4.65rem] w-fit   tracking-wider lg:text-[60px] ">
+            Seminar Teknologi &<br />
+            Berbagi Pengalaman
+          </h1>
+          <p className="lg:w-[70%] -mt-2 text-lg lg:text-xl text-[#DEDEDE] dark:text-[#D0D0D0]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            vulputate libero et velit interdum, ac aliquet odio mattis. et velit
+            interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex gap-x-6">
+            <button
+              className={`text-white  text-base lg:text-xl rounded-3xl font-bold px-10 lg:px-14 py-3 border-none shadow-sm 
+               bg-[#F164F0]/80  dark:bg-[#D54CF0] hover:-translate-y-[6px] duration-300`}
+            >
+              Daftar
+            </button>
+            <button
+              className={`text-white text-sm lg:text-lg rounded-3xl font-bold px-7  lg:px-10 py-3 shadow-sm 
+               bg-transparent  border-[3px] border-white hover:-translate-y-[6px] duration-300`}
+            >
+              Info Lainnya
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- pages/seminar.jsx
+++ pages/seminar.jsx
@@ -1,7 +1,13 @@
+import Jumbotron from "@/components/Seminar/Jumbotron";
 import React from "react";
 
-const seminar = () => {
-  return <div>seminar</div>;
-};
+function seminar() {
+  return (
+    <div className="bg-[#FCF2FF] dark:bg-[#02053E] w-full overflow-x-hidden">
+      <Jumbotron />
+      <div className="mb-96"></div>
+    </div>
+  );
+}
 
 export default seminar;
