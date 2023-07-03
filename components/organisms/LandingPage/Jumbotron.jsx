4693f78a7c0a2fa280d8158fafa99872b6876eba
ZildjianVito <zildjianvito07@gmail.com>
2023-07-04 02:40:46 +0700

    fix big header srifoton landing page

--- components/organisms/LandingPage/Jumbotron.jsx
+++ components/organisms/LandingPage/Jumbotron.jsx
@@ -14,10 +14,15 @@ const Jumbotron = () => {
         }}
       >
         <div className="relative flex h-full px-10 pt-10">
-          <div className="flex flex-col h-full gap-8 mt-16 lg:gap-8 lg:px-10 ">
-            <h1 className="  text-[55px] md:text-[5rem] lg:text-[12rem] tracking-wider z-10  font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#FC39FC] to-[#428AE0] ">
+          <div className="flex flex-col h-full gap-8 mt-20 lg:gap-8 lg:px-10">
+            {/* <h1
+              className={`text-[2.75rem] md:text-[7rem] lg:text-[10rem] tracking-wider z-10  font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#FC39FC] to-[#428AE0] srifoton-big-header ${ethnocentric.className} `}
+            >
               SRIFOTON
-            </h1>
+            </h1> */}
+            <div className="z-10">
+              <img src="./SRIFOTON.png" className="lg:w-[85%] py-4" />
+            </div>
             <h3 className="text-xl md:text-2xl lg:text-2xl z-[1] lg:w-[55%] text-[#797979] dark:text-[#CFCFCF]  lg:-mt-14 ">
               Sriwijaya Informatics Exhibition atau yang biasa dikenali dengan
               Srifoton adalah forum kompetitif berskala nasional yang mewadahi
