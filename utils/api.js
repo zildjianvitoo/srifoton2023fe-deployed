4e1d2270583ebf97f151149621205cc53e9ab3b8
ZildjianVito <zildjianvito07@gmail.com>
2023-07-10 18:17:48 +0700

    make apicall logic

--- utils/api.js
+++ utils/api.js
@@ -149,6 +149,291 @@ async function doSeminarRegistration({
   return { data };
 }
 
+async function doMobileLegendRegistration({
+  team_name,
+  email,
+  name1,
+  nim1,
+  college1,
+  phone_number1,
+  instagram1,
+  id_mole1,
+  id_card1,
+  name2,
+  nim2,
+  college2,
+  phone_number2,
+  instagram2,
+  id_mole2,
+  id_card2,
+  name3,
+  nim3,
+  college3,
+  phone_number3,
+  instagram3,
+  id_mole3,
+  id_card3,
+  name4,
+  nim4,
+  college4,
+  phone_number4,
+  instagram4,
+  id_mole4,
+  id_card4,
+  name5,
+  nim5,
+  college5,
+  phone_number5,
+  instagram5,
+  id_mole5,
+  id_card5,
+  name6,
+  nim6,
+  college6,
+  phone_number6,
+  instagram6,
+  id_mole6,
+  id_card6,
+  proof,
+  payment_method,
+}) {
+  const { data, status } = await api.post(
+    "/api/mobile-legend/register",
+    {
+      team_name,
+      email,
+      name1,
+      nim1,
+      college1,
+      phone_number1,
+      instagram1,
+      id_mole1,
+      id_card1,
+      name2,
+      nim2,
+      college2,
+      phone_number2,
+      instagram2,
+      id_mole2,
+      id_card2,
+      name3,
+      nim3,
+      college3,
+      phone_number3,
+      instagram3,
+      id_mole3,
+      id_card3,
+      name4,
+      nim4,
+      college4,
+      phone_number4,
+      instagram4,
+      id_mole4,
+      id_card4,
+      name5,
+      nim5,
+      college5,
+      phone_number5,
+      instagram5,
+      id_mole5,
+      id_card5,
+      name6,
+      nim6,
+      college6,
+      phone_number6,
+      instagram6,
+      id_mole6,
+      id_card6,
+      proof,
+      payment_method,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+      },
+    }
+  );
+  if (status !== 200) {
+    throw new Error(data.message);
+  }
+
+  return { data };
+}
+
+async function doCompetitiveProgrammingRegistration({
+  team_name,
+  email,
+  college,
+  name1,
+  nim1,
+  phone_number1,
+  instagram1,
+  id_card1,
+  name2,
+  nim2,
+  phone_number2,
+  instagram2,
+  id_card2,
+  name3,
+  nim3,
+  phone_number3,
+  instagram3,
+  id_card3,
+  proof,
+  payment_method,
+}) {
+  const { data, status } = await api.post(
+    "/api/competitive-programming/register",
+    {
+      team_name,
+      email,
+      college,
+      name1,
+      nim1,
+      phone_number1,
+      instagram1,
+      id_card1,
+      name2,
+      nim2,
+      phone_number2,
+      instagram2,
+      id_card2,
+      name3,
+      nim3,
+      phone_number3,
+      instagram3,
+      id_card3,
+      proof,
+      payment_method,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+      },
+    }
+  );
+  if (status !== 200) {
+    throw new Error(data.message);
+  }
+  return { data };
+}
+
+async function doUiUXRegistration({
+  team_name,
+  email,
+  college,
+  nim1,
+  phone_number1,
+  instagram1,
+  id_card1,
+  name2,
+  nim2,
+  phone_number2,
+  instagram2,
+  id_card2,
+  name3,
+  nim3,
+  phone_number3,
+  instagram3,
+  id_card3,
+  proof,
+  payment_method,
+}) {
+  const { data, status } = await api.post(
+    "/api/ui-ux/register",
+    {
+      team_name,
+      email,
+      college,
+      nim1,
+      phone_number1,
+      instagram1,
+      id_card1,
+      name2,
+      nim2,
+      phone_number2,
+      instagram2,
+      id_card2,
+      name3,
+      nim3,
+      phone_number3,
+      instagram3,
+      id_card3,
+      proof,
+      payment_method,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+      },
+    }
+  );
+}
+
+async function doWebDevelopmentRegistration({
+  team_name,
+  email,
+  college,
+  nim1,
+  phone_number1,
+  instagram1,
+  id_card1,
+  name2,
+  nim2,
+  phone_number2,
+  instagram2,
+  id_card2,
+  name3,
+  nim3,
+  phone_number3,
+  instagram3,
+  id_card3,
+  proof,
+  payment_method,
+}) {
+  const { data, status } = await api.post(
+    "/api/web-development/register",
+    {
+      team_name,
+      email,
+      college,
+      nim1,
+      phone_number1,
+      instagram1,
+      id_card1,
+      name2,
+      nim2,
+      phone_number2,
+      instagram2,
+      id_card2,
+      name3,
+      nim3,
+      phone_number3,
+      instagram3,
+      id_card3,
+      proof,
+      payment_method,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+      },
+    }
+  );
+  if (status !== 200) {
+    throw new Error(data.message);
+  }
+  return { data };
+}
+
 async function sendEmailForgotPassword({ email }) {
   const { data, status } = await api.post("/api/forgot-password", {
     email,
@@ -187,6 +472,7 @@ export {
   doVerifyUser,
   updateDataUser,
   doSeminarRegistration,
+  doMobileLegendRegistration,
   sendEmailForgotPassword,
   doResetPassword,
 };
