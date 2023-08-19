1782798c1b6ed360408b21fa09299f2cf5d80639
ZildjianVito <zildjianvito07@gmail.com>
2023-08-19 22:36:05 +0700

    hidden sponsor and medpart for a while

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,7 @@ export default function Home({}) {
         <Intro />
         <Timeline />
         <Benefit />
-        <Sponsor />
+        {/* <Sponsor /> */}
       </LayoutMain>
     </>
   );
