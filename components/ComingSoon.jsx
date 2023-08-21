e4f3cdc4a223dae7096627538b5e7047bf840c0f
ZildjianVito <zildjianvito07@gmail.com>
2023-08-21 13:56:19 +0700

    style: update space between segera and hadir text

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -18,7 +18,7 @@ export default function ComingSoon() {
           <h1 className="z-10 py-3 text-4xl font-bold tracking-wider text-transparent xs:text-5xl md:text-6xl lg:text-8xl bg-gradient-to-r from-pink-srifoton to-blue-srifoton bg-clip-text comingsoon-header-segera">
             Segera
           </h1>
-          <h1 className="px-2 text-4xl font-bold tracking-widest xs:text-5xl md:text-6xl -translate-y-7 lg:-translate-y-12 lg:text-8xl comingsoon-header-hadir dark:comingsoon-header-hadir-dark">
+          <h1 className="px-2 text-4xl font-bold tracking-widest -translate-y-5 xs:text-5xl md:text-6xl lg:-translate-y-7 lg:text-8xl comingsoon-header-hadir dark:comingsoon-header-hadir-dark">
             Hadir
           </h1>
         </div>
