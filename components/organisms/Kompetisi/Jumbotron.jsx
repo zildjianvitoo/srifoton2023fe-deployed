15028694e67e013050550ec47c1eb51b6a5d7b25
ZildjianVito <zildjianvito07@gmail.com>
2023-08-23 11:20:11 +0700

    fix: kompetisi jumbotron for lg breakpoint view

--- components/organisms/Kompetisi/Jumbotron.jsx
+++ components/organisms/Kompetisi/Jumbotron.jsx
@@ -16,10 +16,10 @@ export default function Jumbotron() {
 
   return (
     <>
-      <section className="relative h-full xs:h-screen mt-12 overflow-hidden lg:min-h-[90vh] lg:mt-6">
-        <div className="flex flex-col justify-center p-8 mt-12 lg:px-20 xl:px-32 lg:mt-32 lg:flex-row">
+      <section className="relative h-full mt-12 overflow-hidden xs:h-screen md:h-full lg:min-h-screen xl:min-h-[90vh] lg:mt-6">
+        <div className="flex flex-col justify-center p-8 mt-12 lg:px-20 xl:px-32 lg:mt-32 md:flex-row">
           <div className="flex flex-col justify-center w-fit lg:w-1/2">
-            <h1 className="p-[2px] text-3xl xs:text-4xl font-bold leading-snug lg:leading-[4.65rem] tracking-wide w-fit  lg:text-[60px] jumbotron-kompetisi-header dark:jumbotron-kompetisi-header-dark ">
+            <h1 className="p-[2px] text-3xl xs:text-4xl md:text-4xl font-bold leading-snug lg:leading-[4.65rem] tracking-wide w-fit  lg:text-[60px] jumbotron-kompetisi-header dark:jumbotron-kompetisi-header-dark ">
               Ajang Kompetisi <br /> & Raih Prestasi
             </h1>
             <p className="mt-3 lg:!leading-8 text-base xs:text-lg lg:text-xl text-[#767676] dark:text-[#B4C0D5] lg:w-[80%]">
@@ -30,7 +30,7 @@ export default function Jumbotron() {
             </p>
           </div>
 
-          <div className="relative z-0 flex mt-10 lg:mt-0 lg:w-1/2 ">
+          <div className="relative z-0 flex mt-10 md:mt-5 lg:mt-12 xl:mt-5 lg:w-1/2 ">
             <div className="z-10 flex mx-auto -translate-x-10 xs:-translate-x-7 lg:translate-x-0 lg:absolute -top-10 xl:left-16">
               <SwiperKompetisiCard />
               <div className="flex translate-y-56 -translate-x-28 ">
@@ -64,7 +64,7 @@ export default function Jumbotron() {
                 alt={"pink circle"}
                 width={350}
                 height={350}
-                className="absolute bottom-0 right-0 lg:-translate-y-10 xl:-translate-y-0 lg:translate-x-20 xl:translate-x-8 z-1"
+                className="absolute bottom-0 right-0 lg:-translate-y-12 xl:-translate-y-8 lg:translate-x-20 xl:translate-x-8 z-1"
               />
             </div>
           </div>
