58a3d1e4e5326d655aee9b0df0a4960216265f55
ZildjianVito <zildjianvito07@gmail.com>
2023-09-28 00:44:20 +0700

    style: decrease gap between text and jumbotron seminar

--- components/organisms/Seminar/Jumbotron.jsx
+++ components/organisms/Seminar/Jumbotron.jsx
@@ -13,7 +13,7 @@ export default function Jumbotron() {
           backgroundImage: "url(assets/Seminar/jumbotron-seminar-bgimg.svg)",
         }}
       ></div>
-      <div className="flex flex-col-reverse w-full h-full gap-20 p-8 mt-12 lg:mt-4 xl:mt-16 lg:flex-row">
+      <div className="flex flex-col-reverse w-full h-full p-8 mt-12 gap-14 lg:mt-4 xl:mt-16 lg:flex-row">
         <div className="flex items-center xl:-translate-y-14 lg:w-1/2">
           <Image
             priority
